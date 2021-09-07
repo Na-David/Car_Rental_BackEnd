4868964789cb1ba05a0d941c602950097417576e
hna206@naver.com <hna206@naver.com>
2021-09-08 09:29:33 +1200

    Add project files.

---
+++ Car_Rental_BackEnd/Models/Car_Details.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Rental_BackEnd.Models
+{
+    public class Car_Details
+    {
+        [Key]
+        public string RegNumber { get; set; }
+        public string Car_Maker { get; set; }
+        public string Car_Model { get; set; }
+        public int Car_Mileage { get; set; }
+
+    }
+}
